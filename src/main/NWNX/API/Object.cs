50442398192575090e04dd9c390602ebb2858fec
urothis <natecaple@gmail.com>
2020-08-10 14:52:54 -0500

    donked a file here

--- src/main/NWNX/API/Object.cs
+++ src/main/NWNX/API/Object.cs
@@ -7,54 +7,33 @@ namespace NWNX.API
 {
   public static class Object
   {
-    static Object()
-    {
-      PluginUtils.AssertPluginExists<ObjectPlugin>();
-    }
+    static Object() => PluginUtils.AssertPluginExists<ObjectPlugin>();
 
-    public static void AddToArea(this NwGameObject gameObject, Location location)
-    {
+    public static void AddToArea(this NwGameObject gameObject, Location location) => 
       ObjectPlugin.AddToArea(gameObject, location.Area, location.Position);
-    }
 
-    public static void AddToArea(this NwGameObject gameObject, NwArea area, Vector3 position)
-    {
+    public static void AddToArea(this NwGameObject gameObject, NwArea area, Vector3 position) => 
       ObjectPlugin.AddToArea(gameObject, area, position);
-    }
 
-    public static void AcquireItem(this NwGameObject gameObject, NwItem item)
-    {
+    public static void AcquireItem(this NwGameObject gameObject, NwItem item) => 
       ObjectPlugin.AcquireItem(gameObject, item);
-    }
 
-    public static bool CheckFit(this NwGameObject gameObject, NwItem item)
-    {
-      return CheckFit(gameObject, item.BaseItemType);
-    }
+    public static bool CheckFit(this NwGameObject gameObject, NwItem item) => 
+      CheckFit(gameObject, item.BaseItemType);
 
-    public static bool CheckFit(this NwGameObject gameObject, BaseItemType baseItemType)
-    {
-      return ObjectPlugin.CheckFit(gameObject, (int) baseItemType).ToBool();
-    }
+    public static bool CheckFit(this NwGameObject gameObject, BaseItemType baseItemType) => 
+      ObjectPlugin.CheckFit(gameObject, (int) baseItemType).ToBool();
 
-    public static string GetPersistentString(this NwObject obj, string key)
-    {
-      return ObjectPlugin.GetString(obj, key);
-    }
+    public static string GetPersistentString(this NwObject obj, string key) => 
+      ObjectPlugin.GetString(obj, key);
 
-    public static void SetPersistentString(this NwObject obj, string key, string value)
-    {
+    public static void SetPersistentString(this NwObject obj, string key, string value) => 
       ObjectPlugin.SetString(obj, key, value, true.ToInt());
-    }
 
-    public static bool GetIsStatic(this NwPlaceable placeable)
-    {
-      return ObjectPlugin.GetPlaceableIsStatic(placeable).ToBool();
-    }
+    public static bool GetIsStatic(this NwPlaceable placeable) => 
+      ObjectPlugin.GetPlaceableIsStatic(placeable).ToBool();
 
-    public static void SetIsStatic(this NwPlaceable placeable, bool value)
-    {
+    public static void SetIsStatic(this NwPlaceable placeable, bool value) => 
       ObjectPlugin.SetPlaceableIsStatic(placeable, value.ToInt());
-    }
   }
 }
